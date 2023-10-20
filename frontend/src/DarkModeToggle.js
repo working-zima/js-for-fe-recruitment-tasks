b1de8809104aaa44d2a12e9491f1a852f8394bb0
working-zima <working.zima@gmail.com>
2023-10-20 10:32:20 +0900

    feat: 1-1_레이아웃, 기본 API 요청

---
+++ frontend/src/DarkModeToggle.js
@@ -0,0 +1,34 @@
+class DarkModeToggle {
+  isDarkMode = null;
+
+  constructor({ $target }) {
+    const $wrapper = document.createElement("section");
+    const $DarkModeToggle = document.createElement("input");
+    this.$DarkModeToggle = $DarkModeToggle;
+    this.$DarkModeToggle.type = "checkbox";
+
+    $DarkModeToggle.className = "DarkModeToggle";
+    $wrapper.appendChild($DarkModeToggle);
+    $target.appendChild($wrapper);
+
+    $DarkModeToggle.addEventListener("change", (e) => {
+      this.setColorMode(e.target.checked);
+    });
+
+    this.initColorMode();
+  }
+
+  initColorMode() {
+    // isDarkMode state, checkbox 상태 초기화
+    this.isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    this.$DarkModeToggle.checked = this.isDarkMode;
+    this.setColorMode(this.isDarkMode);
+  }
+
+  setColorMode(isDarkMode) {
+    document.documentElement.setAttribute(
+      "color-mode",
+      isDarkMode ? "dark" : "light"
+    );
+  }
+}
